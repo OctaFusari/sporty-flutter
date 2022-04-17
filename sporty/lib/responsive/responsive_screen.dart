57308f592a7986ad7fa2c4ac049aac372acc9449
octavian.fusari <octavian.fusari@marconirovereto.it>
2022-04-17 20:28:34 +0200

    dom 2

---
+++ sporty/lib/responsive/responsive_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:sporty/utils/dimension.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ResponsiveLayout({
+    Key? key,
+    required this.mobileScreenLayout,
+    required this.webScreenLayout,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth > webScreenSize) {
+        // 600 can be changed to 900 if you want to display tablet screen with mobile screen layout
+        return webScreenLayout;
+      }
+      return mobileScreenLayout;
+    });
+  }
+}
